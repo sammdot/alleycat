47c84046baa49f18960b110236c9ac0df6c8ca07
Sammi De Guzman <sammi@sammdot.ca>
2022-08-06 03:17:46 -0700

    Add keyboard shortcuts for paragraph style commands

--- src/extensions/paragraph.ts
+++ src/extensions/paragraph.ts
@@ -1,5 +1,13 @@
 import BaseParagraph from "@tiptap/extension-paragraph"
 
+declare module "@tiptap/core" {
+  interface Commands {
+    customExtension: {
+      setParagraphStyle: (style: string) => boolean
+    }
+  }
+}
+
 export const Paragraph = BaseParagraph.extend({
   addAttributes() {
     return {
@@ -21,7 +29,21 @@ export const Paragraph = BaseParagraph.extend({
         (style: string) =>
         ({ commands }: any) => {
           commands.updateAttributes("paragraph", { style })
+          return true
         },
+    }
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      "Alt-z": () => this.editor.commands.setParagraphStyle("normal"),
+      "Alt-q": () => this.editor.commands.setParagraphStyle("question"),
+      "Alt-a": () => this.editor.commands.setParagraphStyle("answer"),
+      "Alt-k": () => this.editor.commands.setParagraphStyle("colloquy"),
+      "Alt-b": () => this.editor.commands.setParagraphStyle("byline"),
+      "Alt-c": () => this.editor.commands.setParagraphStyle("centered"),
+      "Alt-p": () => this.editor.commands.setParagraphStyle("paren"),
+      "Alt-x": () => this.editor.commands.setParagraphStyle("quoted"),
     }
   },
 })
