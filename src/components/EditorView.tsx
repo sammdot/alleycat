8767785b48f47bda868a9bd4179673a411fcb720
Sammi De Guzman <sammi@sammdot.ca>
2022-08-10 20:07:12 -0700

    Fix styling when editor view is taller than window

--- src/components/EditorView.tsx
+++ src/components/EditorView.tsx
@@ -10,7 +10,7 @@ export function EditorView({ editor }: Props) {
     <div className="grow">
       <EditorContent
         editor={editor}
-        className="h-full overflow-x-hidden overflow-y-scroll"
+        className="h-full overflow-x-hidden overflow-y-scroll bg-gray-50 dark:bg-gray-700 text-gray-600 dark:text-gray-200 font-mono leading-loose break-words"
       />
     </div>
   )
