6a53ef532792e70b3d95ff6947c2cc300a4b5195
Sammi De Guzman <sammi@sammdot.ca>
2022-08-10 20:20:58 -0700

    Clean up connections on Plover side

--- alleycat_link/link.py
+++ alleycat_link/link.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import os
 import os.path
+from plover import log
 import sys
 import threading
 
@@ -17,13 +18,37 @@ class LinkHandler:
     self._all_clients = lst
     self._writer = None
 
-  def __call__(self, _, writer):
+  @property
+  def addr(self):
+    return (
+      (
+        self._writer.get_extra_info("peername")
+        or self._writer.get_extra_info("sockname")
+      )
+      if self._writer
+      else None
+    )
+
+  async def __call__(self, reader, writer):
+    log.info(f"Connected to AlleyCAT on {self.addr}")
     self._all_clients.append(self)
+    self._reader = reader
     self._writer = writer
+    await self._loop()
+
+  async def _loop(self):
+    while True:
+      line = await self._reader.readline()
+      if not line:
+        break
+
+    log.info(f"AlleyCAT on {self.addr} disconnected")
+    self._writer.close()
+    await self._writer.wait_closed()
+    self._all_clients.remove(self)
 
   async def send(self, obj):
     if self._writer.is_closing():
-      self._all_clients.remove(self)
       return
     self._writer.write(json.dumps(obj).encode("utf-8"))
     self._writer.write(b"\r\n")
@@ -40,15 +65,18 @@ class Link:
 
   async def _start_server(self):
     if sys.platform == "win32":
+      host, port = TCP_ADDRESS
       self._server = await asyncio.start_server(
-        LinkHandler(self._clients), *TCP_ADDRESS
+        LinkHandler(self._clients), host, port
       )
+      log.info(f"Started AlleyCAT Link server on {host}:{port}")
     else:
       if os.path.exists(UNIX_ADDRESS):
         os.remove(UNIX_ADDRESS)
       self._server = await asyncio.start_unix_server(
         LinkHandler(self._clients), UNIX_ADDRESS
       )
+      log.info(f"Started AlleyCAT Link server on {UNIX_ADDRESS}")
     async with self._server:
       await self._server.serve_forever()
 
