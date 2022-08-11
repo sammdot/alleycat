80cd6ac75d4c5c093ba6567b97704dceb2bd5a56
Sammi De Guzman <sammi@sammdot.ca>
2022-08-10 20:46:06 -0700

    Implement steno data linkage for outlines that only insert text without newlines

--- src/extensions/link.ts
+++ src/extensions/link.ts
@@ -16,16 +16,99 @@ export const PloverLink = Extension.create({
     return {
       locateTranslation:
         (data: LinkData) =>
-        ({ commands }: any) => {
+        ({ editor, commands: c }: any) => {
           // TODO: The input was entered in AlleyCAT; find how much was entered
           // and wrap that in a translation object.
+          let { timestamp, translated, sent, stroked, is_correction } = data
+          let { from } = translated
+          if (from.length !== 0) {
+            // TODO
+            return true
+          }
+
+          if (is_correction) {
+            return true
+          }
+
+          let outputEnd = editor.state.selection.$head.pos
+
+          for (let item of sent) {
+            if (item.type === "string") {
+              let { string: text } = item
+
+              if (text.includes("\n")) {
+                // TODO
+                continue
+              }
+
+              let length = text.length
+              c.setTextSelection({
+                from: outputEnd - length,
+                to: outputEnd + 1,
+              })
+              c.insertContent([
+                {
+                  type: "translation",
+                  content: [
+                    { type: "stroke", attrs: { steno: stroked, timestamp } },
+                    { type: "text", text },
+                  ],
+                },
+              ])
+            } else if (item.type === "backspaces") {
+              // TODO
+              continue
+            } else if (item.type === "key_combo") {
+              // Ignore key combinations on purpose
+              continue
+            }
+          }
+
           return true
         },
       addTranslation:
         (data: LinkData) =>
-        ({ commands }: any) => {
+        ({ commands: c }: any) => {
           // TODO: The input was entered outside AlleyCAT; create a translation
           // object and add it to the document.
+          let { timestamp, translated, sent, stroked, is_correction } = data
+          let { from } = translated
+          if (from.length !== 0) {
+            // TODO
+            return true
+          }
+
+          if (is_correction) {
+            return true
+          }
+
+          for (let item of sent) {
+            if (item.type === "string") {
+              let { string: text } = item
+
+              if (text.includes("\n")) {
+                // TODO
+                continue
+              }
+
+              c.focus("end")
+              c.insertContent([
+                {
+                  type: "translation",
+                  content: [
+                    { type: "stroke", attrs: { steno: stroked, timestamp } },
+                    { type: "text", text },
+                  ],
+                },
+              ])
+            } else if (item.type === "backspaces") {
+              // TODO
+              continue
+            } else if (item.type === "key_combo") {
+              // Ignore key combinations on purpose
+              continue
+            }
+          }
           return true
         },
     }
