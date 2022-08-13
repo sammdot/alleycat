852677e35a649315203ca3b7e57910a5476d6396
Sammi De Guzman <sammi@sammdot.ca>
2022-08-13 00:48:16 -0700

    Add dark theming to Version component

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -1,9 +1,9 @@
 export function Version() {
   return (
-    <div className="select-none pointer-events-none align-right fixed top-3 right-4 text-sm text-gray-400 dark:text-gray-500">
+    <div className="select-none pointer-events-none align-right fixed top-3 right-4 text-sm text-gray-400">
       alleycat-{process.env.ACAT_PLATFORM} v{process.env.ACAT_VERSION}
       {process.env.ACAT_DEBUG && (
-        <span className="uppercase bg-red-400 px-1 ml-2 text-white rounded font-semibold">
+        <span className="uppercase bg-red-400 dark:bg-red-500 px-1 ml-2 text-white rounded font-semibold">
           debug
         </span>
       )}
